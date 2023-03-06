dedd9178aff410172fdb90f5f2d083f44266a619
newzen <starsorbit@naver.com>
2023-03-06 13:49:06 +0900

    [박기석]

---
+++ src/main/java/com/pks/movie/user/UserController.java
@@ -0,0 +1,16 @@
+package com.pks.movie.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@GetMapping("/signup/view")
+	public String singupView() {
+		return "/user/signup";
+	}
+	
+}
